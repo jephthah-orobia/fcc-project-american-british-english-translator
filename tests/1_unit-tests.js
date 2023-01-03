abe9df5c2b5a74aced9040c74c44087d3505ad88
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-01-03 15:43:20 +0800

    unit test: write 24 failing test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,4 +5,157 @@ const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
 
+    suite('Translate to British English:', function () {
+        test("Mangoes are my favorite fruit.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("I ate yogurt for breakfast.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("We had a party at my friend's condo.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Can you toss this in the trashcan for me?",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("The parking lot was full.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Like a high tech Rube Goldberg machine.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("To play hooky means to skip class or work.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("No Mr. Bond, I expect you to die.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Dr. Grosh will see you now.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Lunch is at 12:15 today.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+    });
+
+    suite('Translate to Americal English:', function () {
+
+        test("We watched the footie match for a while.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Paracetamol takes up to an hour to work.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("First, caramelise the onions.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("I spent the bank holiday at the funfair.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("I had a bicky then went to the chippy.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("I've just got bits and bobs in my bum bag.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("The car boot sale at Boxted Airfield was called off.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Have you met Mrs Kalyani?",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Prof Joyner of King's College, London.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Tea time is usually around 4 or 4.30.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+    });
+
+    suite('Highlight translation in:', function () {
+
+        test("Mangoes are my favorite fruit.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("I ate yogurt for breakfast.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("We watched the footie match for a while.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+        test("Paracetamol takes up to an hour to work.",
+            function (done) {
+                assert.fail();
+                done();
+            });
+
+    });
+
 });
