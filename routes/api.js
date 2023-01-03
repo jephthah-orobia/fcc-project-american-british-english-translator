8ca7e89e65a7c073e2951e903eb85f1f4361822e
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2023-01-03 20:26:09 +0800

    implement route

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,20 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
+  const locales = ['american-to-british', 'british-to-american'];
   app.route('/api/translate')
     .post((req, res) => {
-      
+      let text = req.body?.text;
+      let locale = req.body?.locale;
+      if (text == undefined || locale == undefined)
+        return res.json({ error: 'Required field(s) missing' });
+      if (text == '' || text == null)
+        return res.json({ error: 'No text to translate' });
+      if (!locales.includes(locale))
+        return res.json({ error: 'Invalid value for locale field' });
+      return res.json({ text: text, translation: locales[0] == locale ? translator.toBritish(text) : translator.toAmerican(text) });
     });
 };
